6cf264e25bf04812b216595e51d30d181c21999d
SOUMIKBOSE650 <soumikbose650@gmail.com>
2024-12-26 14:03:41 +0530

    2nd commit

--- controller.py
+++ controller.py
@@ -1,8 +1,24 @@
 import time
 from fastapi import FastAPI
 import app as llm_service # Assuming `app.llm_generator(query)` is defined in this module
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# set cors policy
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
+
+@app.get('/ping')
+def ping():
+    return {"message": "pong"}
+
 
 @app.get('/chat/{query}')
 def test(query: str):
